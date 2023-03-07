52558056300b57bd0adaf418b253128dbdec1d5e
佳波 王 <ruanyuanwangjiabo@outlook.com>
2023-03-07 13:48:37 +0800

    添加了本机网卡信息拉取代码

--- ArpWork/ArpWork.cpp
+++ ArpWork/ArpWork.cpp
@@ -1,4 +1,12 @@
 #include "ArpWork.h"
+#include <WinSock2.h>
+#include <Iphlpapi.h>
+#include <iostream>
+#include <sstream>
+#include <string> 
+
+using namespace std;
+#pragma comment(lib,"Iphlpapi.lib")
 
 ArpWork::ArpWork(QWidget *parent)
     : QMainWindow(parent)
@@ -19,5 +27,81 @@ void ArpWork::send()
 }
 
 void ArpWork::gethost() {
+    //PIP_ADAPTER_INFO结构体指针存储本机网卡信息
+    PIP_ADAPTER_INFO pIpAdapterInfo = new IP_ADAPTER_INFO();
+    //得到结构体大小,用于GetAdaptersInfo参数
+    unsigned long stSize = sizeof(IP_ADAPTER_INFO);
+    //调用GetAdaptersInfo函数,填充pIpAdapterInfo指针变量;其中stSize参数既是一个输入量也是一个输出量
+    int nRel = GetAdaptersInfo(pIpAdapterInfo, &stSize);
+    //记录网卡数量
+    int netCardNum = 0;
+    //记录每张网卡上的IP地址数量
+    int IPnumPerNetCard = 0;
+    if (ERROR_BUFFER_OVERFLOW == nRel) {
+        //如果函数返回的是ERROR_BUFFER_OVERFLOW
+        //则说明GetAdaptersInfo参数传递的内存空间不够,同时其传出stSize,表示需要的空间大小
+        //这也是说明为什么stSize既是一个输入量也是一个输出量
+        //释放原来的内存空间
+        delete pIpAdapterInfo;
+        //重新申请内存空间用来存储所有网卡信息
+        pIpAdapterInfo = (PIP_ADAPTER_INFO)new BYTE[stSize];
+        //再次调用GetAdaptersInfo函数,填充pIpAdapterInfo指针变量
+        nRel = GetAdaptersInfo(pIpAdapterInfo, &stSize);
+    }
+    if (ERROR_SUCCESS == nRel) {
+        //输出网卡信息
+         //可能有多网卡,因此通过循环去判断
+        QString NetName, NetIP, NetMAC;
+        while (pIpAdapterInfo) {
 
+            netCardNum++;
+            
+            string count;
+            stringstream sstr;
+            sstr << netCardNum;
+            count = sstr.str();
+
+            NetName += "Net";
+            NetName += count;
+            NetName += " ";
+            NetName += pIpAdapterInfo->Description;
+            NetName += "\n";
+
+            ui.plainTextEdit->setPlainText(NetName);
+
+            char mac[128];
+            sprintf_s(mac, 18, "%02X-%02X-%02X-%02X-%02X-%02X",
+                pIpAdapterInfo->Address[0],
+                pIpAdapterInfo->Address[1],
+                pIpAdapterInfo->Address[2],
+                pIpAdapterInfo->Address[3],
+                pIpAdapterInfo->Address[4],
+                pIpAdapterInfo->Address[5]
+            );
+            NetMAC += "MAC ";
+            NetMAC += mac;
+            NetMAC += "\n";
+
+            ui.plainTextEdit_2->setPlainText(NetMAC);
+
+            //可能网卡有多IP,因此通过循环去判断
+            IP_ADDR_STRING* pIpAddrString = &(pIpAdapterInfo->IpAddressList);
+            NetIP += "IP ";
+            NetIP += pIpAddrString->IpAddress.String;
+            NetIP += "\n";
+
+            ui.plainTextEdit_3->setPlainText(NetIP);
+
+            //do {
+            //    cout << "IP ：" << pIpAddrString->IpAddress.String << endl;
+           //     pIpAddrString = pIpAddrString->Next;
+           // } while (pIpAddrString);
+
+            pIpAdapterInfo = pIpAdapterInfo->Next;
+        }
+    }
+    //释放内存空间
+    if (pIpAdapterInfo) {
+        delete pIpAdapterInfo;
+    }
 }
